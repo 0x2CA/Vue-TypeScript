53767fc70d65edc3e8ffdf79459b3ec540751d68
0x2CA <2478557459@qq.com>
2020-06-29 11:59:48 +0800

    整理

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -5,6 +5,7 @@ import routes from "../routes"
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+    mode: 'history', //https://router.vuejs.org/zh/guide/essentials/history-mode.html
     routes
 });
 
